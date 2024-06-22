71430a1447cd67495c89d71e6d912e20355ee8e6
Jacek W <wozjac@zoho.com>
2024-06-22 20:23:32 +0200

    build: bump `@types/node`

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
 import { TextEncoder } from "util";
 
 let projPath: string;
